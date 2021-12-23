d23823e29ce65fd5c81eba9d2f01858f58f93cb1
unknown <paullewa15@gmail.com>
2021-12-23 22:50:06 +0100

    'app.py'

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, jsonify, request
+import re
+from flask import Flask, jsonify,request
+
 
 # Intitialise the app
 app = Flask(__name__)
 
 # Define what the app does
-@app.get("/greet")
+
+@app.route("/greet",methods=['GET'] )
 def index():
-    """
-    TODO:
-    1. Capture first name & last name
-    2. If either is not provided: respond with an error
-    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
-    """
-    return jsonify("TODO")
+    fname = request.args.get('fname')
+    lname = request.args.get("lname")
+    if not fname and not lname:
+        return jsonify({ "status" : "error" })
+    elif lname and not fname:
+        
+        return jsonify({"data":f" Hello Mr {lname}!"})
+    elif  not lname and fname:
+        return jsonify({'data':f'Hallo  {fname}'})
+    else:
+        return jsonify({'data':f'Is your name {fname} {lname}?'})
